4ee44e43cd51d2b55b755b278803083ac195dfd5
singuruchandini <singuruchandini@gmail.com>
2025-01-24 13:38:01 +0530

    Create Diet.py

---
+++ Diet.py
@@ -0,0 +1,7 @@
+echo "# Ocular-disease-prediction" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/singuruchandini/Ocular-disease-prediction.git
+git push -u origin main
